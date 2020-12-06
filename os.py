95ca0e5a584a87b3391ebf18d092b633e9128b1a
Heejun Shim <hightempler@gmail.com>
2020-12-06 12:30:16 +0900

    update initial version

---
+++ os.py
@@ -0,0 +1,12 @@
+import os
+import datetime
+
+def makeOutDir(outDir=None):
+    if not outDir:
+        dayTime = datetime.now().strftime("%Y%m%d_%H%M%S")
+        outDirName = "Out_" + dayTime
+        cwdPath = os.getcwd()
+        outDir = os.path.join(cwdPath, outDirName)
+    os.mkdir(outDir)
+    print ("Directory '{}' is created ...".format(outDir))
+    return dict(dayTime=dayTime, outDir=outDir)
