1843f7dfc7234193358038e7faa32878590ded26
qocop77 <75548808+qocop77@users.noreply.github.com>
2020-12-07 20:27:48 +0900

    Update os.py

--- os.py
+++ os.py
@@ -2,8 +2,8 @@ import os
 from datetime import datetime
 
 def makeOutDir(outDir=None):
+    dayTime = datetime.now().strftime("%Y%m%d_%H%M%S")
     if not outDir:
-        dayTime = datetime.now().strftime("%Y%m%d_%H%M%S")
         outDirName = "Out_" + dayTime
         cwdPath = os.getcwd()
         outDir = os.path.join(cwdPath, outDirName)
